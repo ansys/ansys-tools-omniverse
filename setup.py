e5cca5ffe3724247e46be1751a3b09d8f4778554
Mario Ostieri <mario.ostieri@ansys.com>
2024-07-30 14:23:46 +0100

    fix package builds

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import os
 import shutil
 
 import setuptools
+import setuptools.command.build
 import setuptools.command.build_py
 from setuptools.command.build_py import build_py as build_py_orig
 import setuptools.command.sdist
@@ -30,8 +31,9 @@ class CustomBuildPy(build_py_orig):
         target_dir = os.path.join(self.build_lib, "ansys", "tools", "omniverse", "exts")
         if not os.path.exists(target_dir):
             os.makedirs(target_dir)
-        for item in os.listdir("exts"):
-            s = os.path.join("exts", item)
+        exts = os.path.join(os.path.dirname(__file__), "exts")
+        for item in os.listdir(exts):
+            s = os.path.join(exts, item)
             d = os.path.join(target_dir, item)
             if os.path.isdir(s):
                 shutil.copytree(s, d, dirs_exist_ok=True)
@@ -43,6 +45,20 @@ class CustomBuildPy(build_py_orig):
 egg_info_dir = "build/egg-info"
 if not os.path.exists(egg_info_dir):
     os.makedirs(egg_info_dir)
+
+
+def package_files(directory):
+    paths = []
+    for path, directories, filenames in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join(path, filename))
+    return paths
+
+
+exts_files = package_files(os.path.join(os.path.dirname(__file__), "exts"))
+
+
+_packages = setuptools.find_namespace_packages("src", include=("ansys.*",))
 
 if __name__ == "__main__":
     setuptools.setup(
@@ -60,7 +76,8 @@ if __name__ == "__main__":
         python_requires=">=3.9",
         install_requires=["ansys-pyensight-core==0.8.8"],
         package_dir={"": "src"},
-        packages=setuptools.find_namespace_packages("src", include=("ansys.*",)),
+        packages=_packages,
         cmdclass={"build_py": CustomBuildPy},
         include_package_data=True,
+        package_data={"": ["VERSION"], "ansys.tools.omniverse": exts_files},
     )
